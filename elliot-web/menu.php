c8ffc42b718e43a8949c7cea8a710acddadc15e3
gabriel <gabriel@znet>
2017-01-21 21:07:38 -0200

    Upgrade Menu

--- elliot-web/menu.php
+++ elliot-web/menu.php
@@ -21,34 +21,40 @@
   /* Menu.php: Displays the main menu */
        
        
-?>   
+?>
 
- <div class="menu"><!-- Aqui começa o menu, cada <li> é um menu -->
-      <ul>
-	<li id="menu_general">
-		 <span title="Main section">Home</span>
-	</li>
+<div class="salles">   
+	<div class="box_menu">
 	
-	<li id="menu_reports">
-		<a href="index.php?section=summary" title="Display the summary"><span title="Reports section">Relatorios</span></a>
-        </li>
+	</div>
+	<div class="menu"><!-- Aqui começa o menu, cada <li> é um menu -->
+	      <ul>
+		<li id="menu_general">
+			 <span title="Main section">Home</span>
+		</li>
 	
-	<li id="menu_find">
-		<a href="index.php?section=find&amp;searchType=user"><span title="Procura de relatórios"> Procurar</span></a>
-<!-- Sub Menus
+		<li id="menu_reports">
+			<a href="index.php?section=summary" title="Gerar Relatórios"><span title="Seção de Relatórios">Relatorios</span></a>
+	        </li>
+	
+		<li id="menu_find">
+			<a href="index.php?section=find&amp;searchType=user"><span title="Procura de relatórios"> Procurar</span></a>
 
-			<ul>
-				<li>
-					<a href="index.php?section=find&amp;searchType=printer" title="Find a printer">Impressoras</a>
-				</li>
-				<li>
-					<a href="index.php?section=find&amp;searchType=user" title="Find a user">Usuários</a>
-				</li>
-          		</ul>
+<!-- Sub Menus Exemplos:
+
+<ul>
+<li>
+<a href="index.php?section=find&amp;searchType=printer" title="Find a printer">Impressoras</a>
+</li>
+<li>
+<a href="index.php?section=find&amp;searchType=user" title="Find a user">Usuários</a>
+</li>
+</ul>
 -->
-	</li>
-	<li>
-	  <span  href="index.php?section=help" title="Ajuda para usar o Elliot">Ajuda</span>
-	</li>   
-      </ul>
-    </div>
+		</li>
+		<li>
+			<span  href="index.php?section=help" title="Ajuda para usar o Elliot">Ajuda</span>
+		</li>   
+	      </ul>
+	</div>
+</div>
